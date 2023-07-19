10c00866e937309a2f873e229293f5dab2a87dd1
JVictorbs03 <135279466+JVictorbs03@users.noreply.github.com>
2023-07-19 08:38:45 -0300

    Add files via upload

--- login.cy.js
+++ login.cy.js
@@ -1,26 +1,16 @@
-
-
-describe("Registrar-se corretamente", () =>{
+describe("Alura", () =>{
 
     beforeEach(() =>{
-        cy.visit("https://alura-fotos.herokuapp.com");       
+        cy.visit("https://www.alura.com.br");       
     })
-    it('verifica mensagens validacao', () =>{
-        cy.contains('a', 'Register now').click();
-        cy.get('input[formcontrolname="email"]').type('johnnycoke1@gmail.com');
-        cy.get('input[formcontrolname="fullName"]').type('João V B Santana')
-        cy.get('input[formcontrolname="userName"]').type('ujao')
-        cy.get('input[formcontrolname="password"]').type('joao062442');
-        cy.contains('button', 'Register').click();
+   
+    it('verifica que está no site correto',() =>{
+        cy.get('.header__nav--logo > img').click();
     })
-    it('Faz login',() =>{
-        cy.contains('a', 'Please, login!').click();
-        cy.get('input[formcontrolname="userName"]').type('ujao');
-        cy.get('input[formcontrolname="password"]').type('joao062442');
-        cy.contains('button', 'login').click();
 
-    })
-    it('Após Login', () =>{
-        cy.get('input[rounded="search..."]').type('johnnycoke1@gmail.com');
+    it('faz login', () =>{
+        cy.contains('a', 'Matricule-se').click();
+        cy.contains('a', 'Entrar').click();
+        cy.contains('h1',('Já estuda com a gente?'));
     })
 })
